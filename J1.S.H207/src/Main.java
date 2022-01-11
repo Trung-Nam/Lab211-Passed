7b81b55517e78014cf06363e09e10d144044a25b
Trung-Nam <82275829+Trung-Nam@users.noreply.github.com>
2022-01-11 22:41:12 +0700

    Add files via upload

---
+++ J1.S.H207/src/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS G731G
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int secondHalfLetters(String str) {
+        int numberChar = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= 'n' && str.charAt(i) <= 'z'
+                    || str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+                numberChar++;
+            }
+        }
+        System.out.println("Number of second halt letters is: " + numberChar);
+
+        return numberChar;
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        secondHalfLetters("ruminates");
+    }
+
+}
